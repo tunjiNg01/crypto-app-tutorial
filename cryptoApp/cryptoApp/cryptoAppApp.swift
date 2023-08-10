c17f9a75b9f300345e200177d07f6b21f82b22ff
tunjiNg01 <nahtlosng@gmail.com>
2023-08-10 14:09:36 +0100

    init

---
+++ cryptoApp/cryptoApp/cryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoAppApp.swift
+//  cryptoApp
+//
+//  Created by MACBOOK PRO on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct cryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
